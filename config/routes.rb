8eb3430847ecd7db163e2b6550f5fde010923acf
Sidinei Silva <sidinei.silva02@gmail.com>
2019-01-31 20:27:16 -0300

    Alterando rota root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'Post#index'
+  root 'posts#index'
 end
