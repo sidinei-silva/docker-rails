e36589ac3c0124e05c32e1c287f25dfcc6efc12c
Sidinei Silva <sidinei.silva02@gmail.com>
2019-01-31 22:57:53 -0300

    Removendo comentario superior

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
-# Um script de ponto de entrada para corrigir um problema específico do Rails que impeça a reinicialização do servidor quando um determinado server.pidarquivo existir. Este script será executado toda vez que o contêiner for iniciado.
-
 #!/bin/bash
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
+
+rake db:create
+rake db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
